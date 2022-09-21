9f30ba60d1e26ba1d3bb3fc25477a32dd8a12c76
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-21 23:00:06 +0200

    Adding style files.

--- functions.php
+++ functions.php
@@ -2,8 +2,10 @@
 
 function university_files()
 {
-    // The first name doesn't matter
-    wp_enqueue_style("university_main_styles", get_stylesheet_uri());
+    // The first name doesn't matter - this is the basic css
+    // wp_enqueue_style("university_main_styles", get_stylesheet_uri());
+    wp_enqueue_style("university_main_styles", get_theme_file_uri("/build/style-index.css"));
+    wp_enqueue_style("university_extra_styles", get_theme_file_uri("/build/index.css"));
 }
 
 add_action("wp_enqueue_scripts", "university_files");
