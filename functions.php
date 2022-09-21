2d4792145bcf8c1f565cfc38b34628e26b431456
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-21 23:09:17 +0200

    Added font awesome icons.

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function university_files()
 {
     // The first name doesn't matter - this is the basic css
     // wp_enqueue_style("university_main_styles", get_stylesheet_uri());
+    wp_enqueue_style("font-awesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css");
     wp_enqueue_style("university_main_styles", get_theme_file_uri("/build/style-index.css"));
     wp_enqueue_style("university_extra_styles", get_theme_file_uri("/build/index.css"));
 }
