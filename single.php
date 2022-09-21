a4dd3a8bdda59216dcca766f590d77c5620a6801
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-21 22:22:04 +0200

    Added header and footer.

--- single.php
+++ single.php
@@ -1,8 +1,13 @@
 <?php
+
+get_header();
+
 while (have_posts()) {
     the_post(); ?>
     <!-- Those two lines are actually enough -->
     <h2><?php the_title(); ?></h2>
     <p><?php the_content(); ?></p>
 <?php
+
+    get_footer();
 }
