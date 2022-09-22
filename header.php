c045a7b60c3fc4eaac36187c3e59d843383ed1fc
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-22 21:01:49 +0200

    Added dynamic title.

--- header.php
+++ header.php
@@ -7,8 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php wp_head() ?>
-
-    <title>Fictional University - created by lilKriT</title>
 </head>
 
 <body>
