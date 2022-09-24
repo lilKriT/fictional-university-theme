ba24b1186c8008a03c089aac8408ee4e34c2fa10
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-24 20:42:59 +0200

    Styled post loop.

--- index.php
+++ index.php
@@ -19,7 +19,16 @@ get_header();
     while (have_posts()) {
         the_post(); ?>
         <div class="post-item">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+            <div class="metabox">
+                <p>Posted by author on day in News</p>
+            </div>
+
+            <div class="generic-content">
+                <?php the_excerpt(); ?>
+                <p><a href="<?php the_permalink(); ?>" class="btn btn--blue">Continue Reading &raquo</a></p>
+            </div>
         </div>
     <?
     }
