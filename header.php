5cc06689f150c2bb048050e429c68d87b1a53c3e
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-24 17:40:06 +0200

    Added language functions. Added body class.

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php wp_head() ?>
 </head>
 
-<body>
+<body class="<?php body_class(); ?>">
 
     <header class="site-header">
         <div class="container">
