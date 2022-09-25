185a560d41482bc6fb99cb810584fa441f70842c
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-25 21:04:03 +0200

    Added metabox to single.

--- single.php
+++ single.php
@@ -19,7 +19,11 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        metabox
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo site_url("/blog"); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time("n.j.y"); ?> in <?php echo get_the_category_list(', '); ?></span>
+            </p>
+        </div>
 
         <div class="generic-content">
             <?php the_content(); ?>
