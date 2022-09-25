e255c7659c3bc47eda677183f6973559e03456f7
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-25 20:53:43 +0200

    Working on singple post page.

--- single.php
+++ single.php
@@ -2,12 +2,31 @@
 
 get_header();
 
+
+
 while (have_posts()) {
     the_post(); ?>
-    <!-- Those two lines are actually enough -->
-    <h2><?php the_title(); ?></h2>
-    <p><?php the_content(); ?></p>
+
+    <!-- Banner -->
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg"); ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p>REPLACE THIS LATER</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+        metabox
+
+        <div class="generic-content">
+            <?php the_content(); ?>
+        </div>
+    </div>
+
 <?php
 
-    get_footer();
 }
+get_footer();
