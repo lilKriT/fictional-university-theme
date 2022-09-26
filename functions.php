3fa71f87229b04c84266e50a5c5572b3669e0d29
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-26 23:02:19 +0200

    Moved the custom post type to a plugin.

--- functions.php
+++ functions.php
@@ -23,15 +23,3 @@ function university_features()
     register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
 }
 add_action("after_setup_theme", "university_features");
-
-function university_post_types()
-{
-    register_post_type("event", array(
-        'public' => true,
-        'labels' => array(
-            'name' => "Events"
-        ),
-        'menu_icon' => "dashicons-calendar"
-    ));
-}
-add_action("init", "university_post_types");
