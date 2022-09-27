a92b49d46350f8b94786809d95277452dd7ced3a
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-27 16:43:37 +0200

    Made an archive for events.

--- single-event.php
+++ single-event.php
@@ -21,7 +21,7 @@ while (have_posts()) {
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
-                <a class="metabox__blog-home-link" href="<?php echo site_url("/event"); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home </a> <span class="metabox__main"><?php the_title(); ?></span>
+                <a class="metabox__blog-home-link" href="<?php echo site_url("/events"); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home </a> <span class="metabox__main"><?php the_title(); ?></span>
             </p>
         </div>
 
