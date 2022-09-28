9e3e3aa5cba1f2b3344c8f5468d320a17f5a52b8
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-28 23:03:55 +0200

    Added two new image sizes. Using the proper image sizes.

--- functions.php
+++ functions.php
@@ -24,6 +24,8 @@ function university_features()
 
     // Featured image
     add_theme_support("post-thumbnails");
+    add_image_size("professorLandscape", 400, 260, true);
+    add_image_size("professorPortrait", 480, 650, true);
 }
 add_action("after_setup_theme", "university_features");
 
--- single-program.php
+++ single-program.php
@@ -56,7 +56,7 @@ while (have_posts()) {
         ?>
                 <li class="professor-card__list-item">
                     <a href="<?php the_permalink(); ?>" class="professor-card">
-                        <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>">
+                        <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
                         <span class="professor-card__name"><?php the_title(); ?></span>
                     </a>
                 </li>
