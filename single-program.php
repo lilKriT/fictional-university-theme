6ed72f1bbea13f208080d63d0903570f90d67482
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-28 21:46:50 +0200

    Listing professors for each subject.

--- single-program.php
+++ single-program.php
@@ -30,6 +30,37 @@ while (have_posts()) {
         </div>
 
         <?php
+
+        $relatedProfessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => "professor",
+            'orderby' => 'title',
+            'order' => "ASC",
+            'meta_query' => array(
+                // I am not sure those quotes are necessary? Works without them.
+                array(
+                    'key' => 'related_programs',
+                    'compare' => "LIKE",
+                    'value' => '"' . get_the_ID() . '"',
+                )
+            )
+        ));
+
+        if ($relatedProfessors->have_posts()) {
+            echo "<hr class='section-break'>";
+            echo "<h2 class='headline headline--medium'>" . get_the_title() . " Professors</h2>";
+
+            while ($relatedProfessors->have_posts()) {
+                $relatedProfessors->the_post();
+        ?>
+                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <?php
+            }
+        }
+
+        // Rewinding the query
+        wp_reset_postdata();
+
         $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
@@ -59,7 +90,7 @@ while (have_posts()) {
 
             while ($homepageEvents->have_posts()) {
                 $homepageEvents->the_post();
-        ?>
+            ?>
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="#">
                         <span class="event-summary__month"><?php
