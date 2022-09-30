e77c91377305b65fa62c5c976bdb9313bb3132e9
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-30 22:55:18 +0200

    You can open and close the search overlay.

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -1,6 +1,26 @@
+import $ from "jquery";
 class Search {
   constructor() {
     // alert("Hi");
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.events();
+  }
+
+  events() {
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverlay.bind(this));
+  }
+
+  openOverlay() {
+    // alert("working");
+    this.searchOverlay.addClass("search-overlay--active");
+  }
+
+  closeOverlay() {
+    // alert("closing");
+    this.searchOverlay.removeClass("search-overlay--active");
   }
 }
 
