2bff148871450907b2bdc0aa0c865b3fa28da9bc
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-16 22:04:09 +0200

    Added a custom field to post type.

--- functions.php
+++ functions.php
@@ -107,3 +107,13 @@ function universityMapKey($api)
     return $api;
 }
 add_filter('acf/field/google_map/api', 'universityMapKey');
+
+function university_custom_rest()
+{
+    register_rest_field("post", "authorName", array(
+        "get_callback" => function () {
+            return get_the_author();
+        }
+    ));
+}
+add_action("rest_api_init", "university_custom_rest");
