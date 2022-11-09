26313fe69c0573f30a44c305e74028d18d40297f
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-09 22:43:31 +0100

    Added styles to log in page.

--- functions.php
+++ functions.php
@@ -141,3 +141,25 @@ function noSubsAdminBar()
     }
 }
 add_action("wp_loaded", "noSubsAdminBar");
+
+// Customize login screen
+function ourHeaderUrl()
+{
+    return esc_url(site_url("/"));
+}
+add_filter("login_headerurl", "ourHeaderUrl");
+
+function ourLoginTitle()
+{
+    return get_bloginfo('name');
+}
+add_filter('login_headertext', 'ourLoginTitle');
+
+function ourLoginCSS()
+{
+    wp_enqueue_style("university_main_styles", get_theme_file_uri("/build/style-index.css"));
+    wp_enqueue_style("university_extra_styles", get_theme_file_uri("/build/index.css"));
+    wp_enqueue_style("font-awesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css");
+    wp_enqueue_style("google-fonts", "https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,400;1,700");
+}
+add_action("login_enqueue_scripts", "ourLoginCSS");
