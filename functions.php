fd65cc7656cc5aa0f57f9930069faad6cbbd189a
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-15 20:07:52 +0100

    Some security changes.

--- functions.php
+++ functions.php
@@ -168,6 +168,11 @@ add_action("login_enqueue_scripts", "ourLoginCSS");
 // Force private notes
 function makeNotePrivate($data)
 {
+    if ($data['post_type'] == "note") {
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+    }
+
     if ($data['post_type'] == "note" && $data['post_status'] != 'trash') {
         $data['post_status'] = "private";
     }
