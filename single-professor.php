5843d23b0faae9d4810f7cfcbb40dec11fd8b283
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-16 19:29:59 +0100

    Displaying likes.

--- single-professor.php
+++ single-professor.php
@@ -18,7 +18,44 @@ while (have_posts()) {
                 <div class="one-third">
                     <?php the_post_thumbnail('professorPortrait'); ?></div>
                 <div class="two-thirds">
-                    <?php the_content(); ?></div>
+                    <?php
+
+                    $likeCount = new WP_Query(array(
+                        "post_type" => "like",
+                        "meta_query" => array(
+                            array(
+                                "key" => "liked_professor_id",
+                                "compare" => "=",
+                                "value" => get_the_id()
+                            )
+                        )
+                    ));
+
+                    $existStatus = "no";
+                    $existQuery = new WP_Query(array(
+                        "author" => get_current_user_id(),
+                        "post_type" => "like",
+                        "meta_query" => array(
+                            array(
+                                "key" => "liked_professor_id",
+                                "compare" => "=",
+                                "value" => get_the_id()
+                            )
+                        )
+                    ));
+
+                    if ($existQuery->found_posts) {
+                        $existStatus = "yes";
+                    }
+
+                    ?>
+                    <span class="like-box" data-exists="<?php echo $existStatus; ?>">
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                        <i class="fa fa-heart" aria-hidden="true"></i>
+                        <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+                    </span>
+                    <?php the_content(); ?>
+                </div>
             </div>
         </div>
 
