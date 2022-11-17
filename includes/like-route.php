22904ae4cd6ab4ac160b7f67cd60a3799c83f577
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-17 18:32:08 +0100

    User can only add one like per professor.

--- includes/like-route.php
+++ includes/like-route.php
@@ -19,14 +19,30 @@ function createLike($data)
     if (is_user_logged_in()) {
         $professor = sanitize_text_field($data["professorID"]);
 
-        return wp_insert_post(array(
+        $existQuery = new WP_Query(array(
+            "author" => get_current_user_id(),
             "post_type" => "like",
-            "post_status" => "publish",
-            "post_title" => "A title for a Like",
-            "meta_input" => array(
-                "liked_professor_id" => $professor
+            "meta_query" => array(
+                array(
+                    "key" => "liked_professor_id",
+                    "compare" => "=",
+                    "value" => $professor
+                )
             )
         ));
+
+        if ($existQuery->found_posts() == 0 && get_post_type($professor) == "professor") {
+            return wp_insert_post(array(
+                "post_type" => "like",
+                "post_status" => "publish",
+                "post_title" => "A title for a Like",
+                "meta_input" => array(
+                    "liked_professor_id" => $professor
+                )
+            ));
+        } else {
+            die("Invalid professor id");
+        }
     } else {
         die("Only logged in users can create a like.");
     }
